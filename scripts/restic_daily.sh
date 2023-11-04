1789e57c0f36b6dd1bf38741df9ae7c597850219
Matthew Elgert <mdelgert@yahoo.com>
2023-11-03 23:06:12 -0400

    remove logging from scripts

--- scripts/restic_daily.sh
+++ scripts/restic_daily.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 
-# Enable or disable redirection to log file
-redirect_output=true
-
-# Generate a Unix timestamp
-timestamp=$(date +"%s")
-
-if [ "$redirect_output" = true ]; then
-  # Define log file path
-  log_file="logs/restic_daily_$timestamp.log"
-
-  # Redirect stdout and stderr to the log file
-  exec > >(tee -a "$log_file")
-  exec 2>&1
-fi
-
-# Log the start of the script
-echo "$timestamp: Starting the backup script"
-
 # Set the repository path
 export RESTIC_REPOSITORY="/mnt/gfs/restic"
 export RESTIC_PASSWORD="password"
@@ -29,15 +11,10 @@ EXCLUDE_DIR="/mnt/d1/docker"
 BACKUP_DIR="/mnt/d1"
 
 # Run the backup
-echo "$timestamp: Running the backup..."
 restic backup $BACKUP_DIR --exclude $EXCLUDE_DIR
 
 # Prune old snapshots
-echo "$timestamp: Pruning old snapshots..."
 restic forget --keep-last 7 --keep-daily 1 --keep-weekly 4 --keep-monthly 12
-
-# Log the completion of the script
-echo "$timestamp: Script completed successfully"
 
 # Exit successfully
 exit 0
--- scripts/rsync_weekly.sh
+++ scripts/rsync_weekly.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-# Enable or disable redirection to log file
-redirect_output=true
-
-# Generate a Unix timestamp
-timestamp=$(date +"%s")
-
-if [ "$redirect_output" = true ]; then
-  # Define log file path
-  log_file="logs/rsync_weekly_$timestamp.log"
-
-  # Redirect stdout and stderr to the log file
-  exec > >(tee -a "$log_file")
-  exec 2>&1
-fi
-
 # Source and destination directories
 source_dir="mdelgert@b1:/mnt/d1/"
 destination_dir="/mnt/gfs/rsync/weekly"
